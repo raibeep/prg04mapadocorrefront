dcc9fbfff614bf9665a69e6c145132e0f3c272d8
raibeep <20242ireads0002@ifba.edu.br>
2026-06-23 21:16:28 -0300

    feat: cria landing page

---
+++ MapaDoCorre/src/modules/landing_page/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Header from "../../../shared/components/Header/Header";
+import Hero from "../components/Hero";
+
+function LandingPage() {
+  return (
+    <>
+      <Header />
+      <Hero />
+    </>
+  );
+}
+
+export default LandingPage;
