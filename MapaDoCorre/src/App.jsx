c562af3356019a47692539a7d3499826412c6ff6
raibeep <20242ireads0002@ifba.edu.br>
2026-06-23 21:22:25 -0300

    rotas atualizadas

--- MapaDoCorre/src/App.jsx
+++ MapaDoCorre/src/App.jsx
@@ -1,29 +1,18 @@
 import { Routes, Route } from 'react-router-dom'
-import Header from './shared/components/Header/Header'
-import Navbar from './shared/components/Navbar/Navbar'
-import Footer from './shared/components/Footer/Footer'
-import Feed from './modules/feed/pages/Feed'
-import Login from './modules/login/pages/Login'
-import Admin from './modules/admin/pages/Admin'
+import Login from './modules/auth/pages/Login'
 import Negocios from './modules/negocios/pages/Negocios'
+import LandingPage from './modules/landing_page/pages/LandingPage'
 
 function App() {
   return (
     <>
-
-      <Header />
-      <Navbar />
-      <div class="pagina">
-        <Routes>
-          <Route path="/" element={<Feed />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/admin" element={<Admin />} />
-          <Route path="/negocios" element={<Negocios />} />
-        </Routes>
-      </div>
-      <Footer />
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/negocios" element={<Negocios />} />
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App
